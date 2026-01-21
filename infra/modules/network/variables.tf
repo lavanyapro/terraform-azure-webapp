8d7a8de273a7137071fdf302db572ba797e625b9
LAVANYA MUDDA <102511405+lavanyapro@users.noreply.github.com>
2026-01-21 16:50:03 +0000

    Add network variables to variables.tf

---
+++ infra/modules/network/variables.tf
@@ -0,0 +1,5 @@
+variable "name" { type = string }
+variable "location" { type = string }
+variable "resource_group_name" { type = string }
+variable "address_space" { type = list(string) }
+variable "subnet_prefixes" { type = list(string) }
